fc9f2c54bb07e80a687fce107ae07a95fb59c30d
andrewquamme <andrewquamme@gmail.com>
2018-10-15 15:45:22 -0700

    Adds comments for daily sales calculations

--- js/app.js
+++ js/app.js
@@ -3,12 +3,14 @@
 var hours = ['6am', '7am', '8am', '9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 
 function getRandomInclusive(min, max) {
+  // receives a min and max as input, calculates a random number inclusive of those numbers
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
 };
 
-function  estimateCustomers(min, max) {
+function  estimateHourlyCustomers(min, max) {
+  // take the estimated min and max customers per hour, and return a random number to estimate the customers per hour for each hour of operation
   var estimates = [];
   for (var i = 0; i < hours.length; i++) {
     var custThisHour = getRandomInclusive(min, max);
@@ -17,22 +19,32 @@ function  estimateCustomers(min, max) {
   return estimates;
 };
 
-function calcCookieSales(hourlyCustArray, averageSales) {
+function calcCookieSales(hourlyCust, averageSales) {
+  // take the array of estimated hourly customers and average cookie sales per customer, and return the estimate of hourly cookie sales
   var hourlySales = [];
-  for (var i = 0; i < hourlyCustArray.length; i++) {
-    var salesThisHour = Math.floor(hourlyCustArray[i] * averageSales);
+  for (var i = 0; i < hourlyCust.length; i++) {
+    var salesThisHour = Math.floor(hourlyCust[i] * averageSales);
     hourlySales.push(salesThisHour);
   };
   return hourlySales;
 };
 
+function calcDailySales(hourlySales) {
+  // take the array of hourly cookie sales and calculate the total sales for the day
+  var totalSales = 0;
+  for (var i = 0; i < hourlySales.length; i++) {
+    totalSales += hourlySales[i];
+  }
+  return totalSales;
+};
+
 var firstPike = {
   locationName: 'First and Pike',
-  minCust: 23,
-  maxCust: 65,
+  minHourlyCust: 23,
+  maxHourlyCust: 65,
   avgSales: 6.3,
 };
 
-firstPike.estCust = estimateCustomers(firstPike.minCust, firstPike.maxCust);
-firstPike.estSales = calcCookieSales(firstPike.estCust, firstPike.avgSales);
-
+firstPike.estHourlyCust = estimateHourlyCustomers(firstPike.minHourlyCust, firstPike.maxHourlyCust);
+firstPike.estHourlySales = calcCookieSales(firstPike.estHourlyCust, firstPike.avgSales);
+firstPike.totalSales = calcDailySales(firstPike.estHourlySales);
